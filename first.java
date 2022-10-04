af5fdd07b05b28381ed3d1848e2e9254589b031a
Amber <ambersinha.5@gmail.com>
2022-10-04 20:33:25 +0530

    Create first.java

---
+++ first.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class first
+{
+    public static void main(String[]args)
+    {
+      Scanner sc=new Scanner (System.in);
+       int num=sc.nextInt();   //take input of no. of last stars to be printed
+       for(int i=1;i<=num;i++){
+       for(int j=num-1;j>=i;j--){
+        System.out.print(" ");
+       }
+       for(int k=1;k<=i;k++){
+        System.out.print("*");}
+        System.out.println();
+        }
+        }
+    }
+    
+    
+    
